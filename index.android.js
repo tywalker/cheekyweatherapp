aa9a9b61e4e0c607c50985e3aeadabded639f497
Tyler Walker <tyler.walker@dakno.com>
2017-06-24 15:46:53 -0400

    Bringing in weather data successfully on a time.

--- index.android.js
+++ index.android.js
@@ -18,9 +18,43 @@ import indexStyles from './src/styles/index';
 import HomeView from './src/homeview';
 
 export default class CheekyWeatherApp extends Component {
+  constructor() {
+    super();
+
+    this._url = 'http://api.openweathermap.org/data/2.5/weather?id=4487042&appid=a8b071b027a6f5c5f2da92477aac2b63';
+    this.state = {
+      payload: null,
+      payloadChange: true,
+      testing: 'TestingOnMount',
+    };
+
+    setInterval(() => {
+      return fetch(this._url)
+        .then((response) => response.json())
+        .then((responseJson) => {
+          // determine payload updates
+          this.setState(previousState => {
+            return { payloadChange: !previousState.payload };
+          });
+
+          console.warn('fired req, received res');
+          // if the payload changes update.
+          if (this.state.payloadChange) {
+            this.setState({
+              payload: responseJson
+            });
+          }
+        })
+        .catch((error) => {
+          console.warn(error);
+        });
+      }, 5000);
+    }
+
   render() {
     return (
       <View>
+       { console.warn(JSON.stringify(this.state.payload)) }
         <HomeView />
       </View>
     );
