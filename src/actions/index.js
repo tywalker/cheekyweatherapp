7ed14092620a9183804d2edbd5e1efc880bf290e
Tyler Walker <tyler.walker@dakno.com>
2017-07-08 12:16:48 -0400

    Added needed actions for geo location.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,9 @@ export const GET_SEARCH_TEXT = 'GET_SEARCH_TEXT'
 export const GET_CITIES_BY_SEARCH = 'GET_CITIES_BY_SEARCH'
 export const RECEIVE_CITIES = 'RECEIVE_CITIES'
 export const ADD_CITY = 'ADD_CITY'
+export const GET_GEO_COORDS = 'GET_GEO_COORDS'
+export const FETCH_FORECAST = 'FETCH_FORECAST'
+export const SHOW_FORECAST = 'SHOW_FORECAST'
 
 export function getSearchText(text) {
   return {
@@ -28,4 +31,18 @@ export function receiveCities(cities) {
 export function addCity(city) {
   type: ADD_CITY,
   city
+}
+
+export function getGeoCoords(geoCoords) {
+  type: GET_GEO_COORDS,
+  geoCoords
+}
+
+export function fetchForecast(forecast) {
+  type: FETCH_FORECAST,
+  forecast
+}
+
+export function showForecast(geoLoc) {
+  type: SHOW_FORECAST
 }
