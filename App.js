c405e7c500b2160c10f46944cb1a13050e8cd60b
Tyler Walker <tyler.walker@dakno.com>
2017-07-19 23:39:20 -0400

    Exported app to expo.

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+
+import AppContainer from './src/containers/AppContainer'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import rootReducer from './src/reducers';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(ReduxThunk)
+);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={ store }>
+        <AppContainer />
+      </Provider>
+    )
+    
+  }
+}
